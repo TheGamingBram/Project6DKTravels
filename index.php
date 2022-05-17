f45ce0648421a1c24c5e7fece06f0d635a6cbae2
TheGamingBram <74347226+TheGamingBram@users.noreply.github.com>
2022-05-17 10:13:41 +0200

    Landing Page
     Needs Text Editing

--- index.php
+++ index.php
@@ -2,7 +2,270 @@
         include("./Assets/config.php"); //connection to database and some test functions
         include("./Assets/header.php"); //insert to bootstrap and other java scripts
 ?>
-<body>
-    <h1>Hello World</h1>
-    <i class="fas fa-qrcode" style="font-size: 1000px;"></i>
+<body class="">
+
+        <!-- NAVBAR START -->
+        <nav class="navbar navbar-expand-lg py-lg-3 navbar-dark">
+            <div class="container">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="mdi mdi-menu"></i>
+                </button>
+
+                <!-- menus -->
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+                    <!-- left menu -->
+                    <ul class="navbar-nav me-auto align-items-center">
+                        <li class="nav-item mx-lg-1">
+                            <a class="nav-link active" href="">Home</a>
+                        </li>
+                        <li class="nav-item mx-lg-1">
+                            <a class="nav-link" href="">Features</a>
+                        </li>
+                        <li class="nav-item mx-lg-1">
+                            <a class="nav-link" href="">Pricing</a>
+                        </li>
+                        <li class="nav-item mx-lg-1">
+                            <a class="nav-link" href="">FAQs</a>
+                        </li>
+                        <li class="nav-item mx-lg-1">
+                            <a class="nav-link" href="">Clients</a>
+                        </li>
+                        <li class="nav-item mx-lg-1">
+                            <a class="nav-link" href="">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- NAVBAR END -->
+
+        <!-- START HERO -->
+        <section class="hero-section">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-5">
+                        <div class="mt-md-4">
+                                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                                        <div class="carousel-inner">
+                                          <div class="carousel-item active" data-bs-interval="10000">
+                                            <img src="Assets/Img/rotate1.png" class="d-block w-100 ImageCarosel " alt="">
+                                          </div>
+                                          <div class="carousel-item" data-bs-interval="2000">
+                                            <img src="Assets/Img/rotate2.png" class="d-block w-100 ImageCarosel" alt="">
+                                          </div>
+                                          <div class="carousel-item">
+                                            <img src="Assets/Img/rotate3.png" class="d-block w-100 ImageCarosel" alt="">
+                                          </div>
+                                          <div class="carousel-item">
+                                            <img src="Assets/Img/rotate4.png" class="d-block w-100 ImageCarosel" alt="">
+                                          </div>
+                                          <div class="carousel-item">
+                                            <img src="Assets/Img/rotate5.png" class="d-block w-100 ImageCarosel" alt="">
+                                         </div>
+                                        </div>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                          <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                          <span class="visually-hidden">Next</span>
+                                        </button>
+                                      </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5 offset-md-2">
+                        <div class="text-md-end mt-3 mt-md-0">
+                            <img src="Assets/Img/Logo.png" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- END HERO -->
+
+        <!-- START SERVICES -->
+        <section class="py-5">
+            <div class="container">
+                <div class="row py-4">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <h1 class="mt-0"><i class="mdi mdi-infinity"></i></h1>
+                            <h3>The admin is fully <span class="text-primary">responsive</span> and easy to <span class="text-primary">customize</span></h3>
+                            <p class="text-muted mt-2">The clean and well commented code allows easy customization of the
+                                theme.It's designed for
+                                <br>describing your app, agency or business.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="text-center p-2 p-sm-3">
+                            <div class="avatar-sm m-auto">
+                                <span class="avatar-title bg-primary-lighten rounded-circle">
+                                    <i class="uil uil-desktop text-primary font-24"></i>
+                                </span>
+                            </div>
+                            <h4 class="mt-3">Responsive Layouts</h4>
+                            <p class="text-muted mt-2 mb-0">Et harum quidem rerum as expedita distinctio nam libero tempore
+                                cum soluta nobis est cumque quo.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="text-center p-2 p-sm-3">
+                            <div class="avatar-sm m-auto">
+                                <span class="avatar-title bg-primary-lighten rounded-circle">
+                                    <i class="uil uil-vector-square text-primary font-24"></i>
+                                </span>
+                            </div>
+                            <h4 class="mt-3">Based on Bootstrap UI</h4>
+                            <p class="text-muted mt-2 mb-0">Temporibus autem quibusdam et aut officiis necessitatibus saepe
+                                eveniet ut sit et recusandae.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="text-center p-2 p-sm-3">
+                            <div class="avatar-sm m-auto">
+                                <span class="avatar-title bg-primary-lighten rounded-circle">
+                                    <i class="uil uil-presentation text-primary font-24"></i>
+                                </span>
+                            </div>
+                            <h4 class="mt-3">Creative Design</h4>
+                            <p class="text-muted mt-2 mb-0">Nam libero tempore, cum soluta a est eligendi minus id quod
+                                maxime placeate facere assumenda est.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="text-center p-2 p-sm-3">
+                            <div class="avatar-sm m-auto">
+                                <span class="avatar-title bg-primary-lighten rounded-circle">
+                                    <i class="uil uil-apps text-primary font-24"></i>
+                                </span>
+                            </div>
+                            <h4 class="mt-3">Multiple Applications</h4>
+                            <p class="text-muted mt-2 mb-0">Et harum quidem rerum as expedita distinctio nam libero tempore
+                                cum soluta nobis est cumque quo.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="text-center p-2 p-sm-3">
+                            <div class="avatar-sm m-auto">
+                                <span class="avatar-title bg-primary-lighten rounded-circle">
+                                    <i class="uil uil-shopping-cart-alt text-primary font-24"></i>
+                                </span>
+                            </div>
+                            <h4 class="mt-3">Ecommerce Pages</h4>
+                            <p class="text-muted mt-2 mb-0">Temporibus autem quibusdam et aut officiis necessitatibus saepe
+                                eveniet ut sit et recusandae.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="text-center p-2 p-sm-3">
+                            <div class="avatar-sm m-auto">
+                                <span class="avatar-title bg-primary-lighten rounded-circle">
+                                    <i class="uil uil-grids text-primary font-24"></i>
+                                </span>
+                            </div>
+                            <h4 class="mt-3">Multiple Layouts</h4>
+                            <p class="text-muted mt-2 mb-0">Nam libero tempore, cum soluta a est eligendi minus id quod
+                                maxime placeate facere assumenda est.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <!-- END SERVICES -->
+
+        
+
+
+
+        <!-- START FOOTER -->
+        <footer class="bg-dark py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <img src="assets/images/logo.png" alt="" class="logo-dark" height="18">
+                        <p class="text-muted mt-4">Hyper makes it easier to build better websites with
+                            <br> great speed. Save hundreds of hours of design
+                            <br> and development by using it.</p>
+
+                        <ul class="social-list list-inline mt-3">
+                            <li class="list-inline-item text-center">
+                                <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
+                            </li>
+                            <li class="list-inline-item text-center">
+                                <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github"></i></a>
+                            </li>
+                        </ul>
+
+                    </div>
+
+                    <div class="col-lg-2 col-md-4 mt-3 mt-lg-0">
+                        <h5 class="text-light">Company</h5>
+
+                        <ul class="list-unstyled ps-0 mb-0 mt-3">
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">About Us</a></li>
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Documentation</a></li>
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Blog</a></li>
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Affiliate Program</a></li>
+                        </ul>
+
+                    </div>
+
+                    <div class="col-lg-2 col-md-4 mt-3 mt-lg-0">
+                        <h5 class="text-light">Apps</h5>
+
+                        <ul class="list-unstyled ps-0 mb-0 mt-3">
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Ecommerce Pages</a></li>
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Email</a></li>
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Social Feed</a></li>
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Projects</a></li>
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Tasks Management</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-2 col-md-4 mt-3 mt-lg-0">
+                        <h5 class="text-light">Discover</h5>
+
+                        <ul class="list-unstyled ps-0 mb-0 mt-3">
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Help Center</a></li>
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Our Products</a></li>
+                            <li class="mt-2"><a href="javascript: void(0);" class="text-muted">Privacy</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="mt-5">
+                            <p class="text-muted mt-4 text-center mb-0">© 2018 - <script>document.write(new Date().getFullYear())</script>2022 Hyper. Design and coded by
+                                Coderthemes</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- END FOOTER -->
 </body>
