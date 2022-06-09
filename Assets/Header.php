7d8e035ce8dbd5d3c9f77af53cf5a2d37b3cb0cb
TheGamingBram <74347226+TheGamingBram@users.noreply.github.com>
2022-06-09 12:06:34 +0200

    Integrate

--- Assets/Header.php
+++ Assets/Header.php
@@ -11,12 +11,12 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css"/>
  
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.css"/>
+ 
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
--- adminpanel.php
+++ adminpanel.php
@@ -7,3 +7,53 @@
         exit;
     }
 ?>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Users</h5>
+    <table id="user_table" class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Naam</th>
+                <th>E-mail</th>
+                <th>Telefoon</th>
+                <th>Registratiedatum</th>
+                <th>Opties</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+                $query_run = mysqli_query($con, "SELECT * FROM klanten");
+                while ($row = mysqli_fetch_assoc($query_run)) 
+                {
+                    ?>
+                    <tr>
+                        <td><?=$row['ID']?></td>
+                        <td><?=$row['Naam']?></td>
+                        <td><?=$row['Email']?></td>
+                        <td><?=$row['Telefoon'] ?></td>
+                        <td><?=$row['Gewijzigd'] ?></td>
+                        <td>
+                            <form action="adminbewerken.php" method="post">
+                                <input type="hidden" name="edit_ID" value="<?php echo $row['ID'] ?>">
+                                <button type="sumbit" name="edit_btn" class="btn btn-primary"> Bewerken</button>
+                            </form>
+                            <form action="admincode.php" method="post">
+                            <input type="hidden" name="delete_ID" value="<?php echo $row['ID']?>">
+                            <button type="sumbit" name="delete_btn" class="btn btn-danger"> Verwijderen</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <?php
+                }
+            ?>
+        </tbody>
+    </table>
+  </div>
+</div>
+
+<script>
+    $(document).ready( function () {
+        $('#user_table').DataTable();
+    } );
+</script>
