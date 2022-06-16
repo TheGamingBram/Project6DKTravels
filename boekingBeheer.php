71931a822d415b755ff03bb196b24d0a37663d6d
rodijanssen <74348802+rodijanssen@users.noreply.github.com>
2022-06-16 10:27:30 +0200

    boeking beheer CRUD systeem

--- boekingBeheer.php
+++ boekingBeheer.php
@@ -23,35 +23,95 @@
 .styled-table tbody tr:nth-of-type(even) {
     background-color: #f3f3f3;
 }
-
-.styled-table tbody tr:last-of-type {
-    border-bottom: 2px solid #009879;
-}
 </style>
 <?php 
         include("./Assets/config.php"); //connection to database and some test functions
         include("./Assets/header.php"); //insert to bootstrap and other java scripts
+        if(isset($_POST['submit'])){
+                
+                $sql = "UPDATE `tochten` SET `Omschrijving` = '" .$_POST['Omschrijving'] . "', `Route` ='" .$_POST['Route'] . "', `Aantaldagen` = '" .$_POST['Aantaldagen'] . "' WHERE `tochten`.`ID` = '" .$_POST['id'] . "';";
+                if (mysqli_query($link, $sql)) {}
+        }
+        if(isset($_GET['aktie'])) {
+
+                $ID = $_GET['id'];
+                $sql = "SELECT * FROM `boekingen` WHERE `boekingen`.`ID` ='" . $ID . "'";
+                        $result2 = mysqli_query($link, $sql);
+                        while($row2 = mysqli_fetch_array($result2)){
+                                $tochtenID = $row2['FKtochtenID'];
+                        }
 
 
-        $query = "SELECT * FROM tochten"; //You don't need a ; like you do in SQL
+                if($_GET['aktie'] == "Update"){
+                        $sql = "SELECT * FROM `tochten` WHERE `tochten`.`ID` ='" . $tochtenID . "'";
+                        $result = mysqli_query($link, $sql);
+
+                        echo "<table class='styled-table'>";
+                        echo "<tr>
+                                <th>ID</th>
+                                <th>Omschrijving status</th>
+                                <th>Route</th>
+                                <th>Aantaldagen</th>
+                                <th>veranderen</th>
+                        </tr>";
+                        while($row = mysqli_fetch_array($result)){
+                                echo "<tr><form action='boekingBeheer.php' method='post'>
+                                        <td><input type='hidden' value='" . $row['ID'] . "' name='id' ><input type='text' value=" . $row['ID'] . " name='id' disabled></td>
+                                        <td><input type='text' value='" . $row['Omschrijving'] . "' name='Omschrijving' required></td>
+                                        <td><input type='text' value='" . $row['Route'] . "' name='Route' required></td>
+                                        <td><input type='text' value='" .  $row['Aantaldagen'] . "' name='Aantaldagen' required></td>
+                                        <td><input type='submit' name='submit' value='versturen' /></form></td>";
+                        }
+                        echo "</table>";
+                }
+                if($_GET['aktie'] == "Delete"){
+
+                        $sql = "DELETE FROM `tochten` WHERE `tochten`.`ID` ='" . $tochtenID . "'";
+                        if (mysqli_query($link, $sql)) {
+                            }
+                        $sql = "DELETE FROM `boekingen` WHERE `boekingen`.`ID` ='" . $ID . "'";
+                        if (mysqli_query($link, $sql)) {
+                            }
+                        
+                }
+        }
+
+        $query = "SELECT * FROM boekingen";
         $result = mysqli_query($link, $query);
 
-        echo "<table class='styled-table'>"; // start a table tag in the HTML
+        echo "<table class='styled-table'>";
         echo "<tr>
                 <th>ID</th>
-                <th>Omschrijving</th>
+                <th>startdatum</th>
+                <th>Omschrijving status</th>
                 <th>Route</th>
                 <th>Aantaldagen</th>
+                <th>klant naam</th>
                 <th>update</th>
                 <th>delete</th>
         </tr>";
-        while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-                echo "<tr><td>" . $row['ID'] . "</td><td>" . $row['Omschrijving'] . "</td><td>" . $row['Route'] . "</td><td>" .  $row['Aantaldagen'] . "</td><td>" . '<a href="http://localhost/Project6DKTravels/boekingBeheer.php?aktie=wijzigen&id='.$row['ID'].'">Update</a>' . "</td><td>" . '<a href="http://localhost/Project6DKTravels/boekingBeheer.php?aktie=delete&id='.$row['ID'].'">delete</a>'. "</td></tr>";
+        while($row = mysqli_fetch_array($result)){
+                echo "<tr><td>" . $row['ID'] . "</td><td>" . $row['Startdatum'] . "</td>";
+                $sql = "SELECT * FROM `tochten` WHERE `tochten`.`ID` ='" . $row['FKtochtenID'] . "'";
+                $result2 = mysqli_query($link, $sql);
+                while($row2 = mysqli_fetch_array($result2)){
+                        echo "<td>" . $row2['Omschrijving'] . "</td>
+                        <td>" . $row2['Route'] . "</td>
+                        <td>" .  $row2['Aantaldagen'] . "</td>";
+                }
+                $sql = "SELECT * FROM `klanten` WHERE `klanten`.`ID` ='" . $row['FKklantenID'] . "'";
+                $result2 = mysqli_query($link, $sql);
+                while($row2 = mysqli_fetch_array($result2)){
+                        echo "<td>" . $row2['Naam'] . "</td>
+                        <td>" . $row2['Email'] . "</td>
+                        <td>" . $row2['Telefoon'] . "</td>";
+                }
+                echo "<td>" . '<a href="http://localhost/Project6DKTravels/boekingBeheer.php?aktie=Update&id='.$row['ID'].'">Update</a>'
+                . "</td><td>" . '<a href="http://localhost/Project6DKTravels/boekingBeheer.php?aktie=Delete&id='.$row['ID'].'">delete</a>'. "</td></tr>";
+
         }
         echo "</table>";
 
-        if(isset($_POST['update'])) {
-                echo "This is Button1 that is selected";
-        }
+        
 
         
