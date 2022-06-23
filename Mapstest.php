ff71826a47989d279e27bc3fa958ba62a81806e2
TheGamingBram <74347226+TheGamingBram@users.noreply.github.com>
2022-06-23 16:05:51 +0200

    Update Mapstest.php

--- Mapstest.php
+++ Mapstest.php
@@ -18,14 +18,15 @@
 
 <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
 <link href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" rel="stylesheet"/>
-
 <script>
     var element = document.getElementById('map');
-    element.style = 'height:300px;';
+    element.style = 'height:500px;';
     var map = L.map(element);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
     var target = L.latLng('51.7218987', '5.8797225');
     map.setView(target, 14);
     L.marker(target).addTo(map);
